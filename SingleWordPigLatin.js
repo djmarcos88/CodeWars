f203e745fc0d5391ae3f59ed8435c8d018d0915b
Marcos Djivelekian <djmarcos88@gmail.com>
2015-06-28 18:38:08 -0300

    Initial Commit

---
+++ SingleWordPigLatin.js
@@ -0,0 +1,16 @@
+function pigLatin(string) {
+  var result;
+
+  console.log(!/^[a-zA-Z]+$/.test(string));
+  if (!/^[a-zA-Z]+$/.test(string)) {
+    result = null;
+  }
+  else if (/^[aeiou]/.test(string)) {
+    result = string + 'way';
+  }
+  else {
+
+  }
+
+  return result;
+}
