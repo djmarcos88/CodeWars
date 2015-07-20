b4f8fa2c0ddab300259362a108f14760770d4852
Marcos Djivelekian <djmarcos88@gmail.com>
2015-07-20 00:00:35 -0300

    Added UnitTesting project to the VS solution.

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class UnitTests
+	{
+		[TestMethod]
+		public void DescendingOrder()
+		{
+			Assert.AreEqual(0, Kata.DescendingOrder(0));
+
+			Assert.AreEqual(321, Kata.DescendingOrder(213));
+
+			Assert.AreEqual(98321, Kata.DescendingOrder(21398));
+
+			Assert.AreEqual(987654321, Kata.DescendingOrder(123456789));
+		} 
+	}
+}
